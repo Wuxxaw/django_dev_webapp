79703f95daf340a8b9245b3f95ba36fd1326a210
wilso <wilsonadam95@gmail.com>
2023-03-23 18:55:06 +0000

    first commit
    Signed-off-by: wilso <wilsonadam95@gmail.com>

---
+++ mysite/polls/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse("Hello World. You're at the Polls index")
+# Create your views here.
+
+
+
